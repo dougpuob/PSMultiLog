e6458d0a066d073b028e7848c266d3e962d88e27
Douglas Chen <dougpuob@gmail.com>
2021-02-19 11:56:59 +0800

    Updated package information.

--- PublishModule.ps1
+++ PublishModule.ps1
@@ -1,10 +1,10 @@
-
 $Manifest = @{
     Path                    = 'PSMultiLogPlus.psd1'
     RootModule              = 'PSMultiLogPlus.psm1'
-    Author                  = 'dougpuob'
+    Author                  = 'Douglas Chen <dougpuob@gmail.com>'
+    Copyright               = 'MIT'
     CompatiblePSEditions    = 'Desktop'
-    ModuleVersion           = '0.0.1'
+    ModuleVersion           = '0.0.3'
     Description             = 'PSMultiLogPlus'
     PowerShellVersion       = '5.1'
     GUID                    = '112a3d06-a510-41c8-a906-50321f3c64ed'
